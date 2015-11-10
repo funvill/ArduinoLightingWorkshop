fe5fcddf453ebe914d237bde1560993bbddbd937
Steven Smethurst <funvill@funvill.com>
2015-11-09 18:28:39 -0800

    Added Serial Analog demo

--- arduino/ButtonCount/ButtonCount.ino
+++ arduino/ButtonCount/ButtonCount.ino
@@ -1,43 +1,40 @@
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
-#define LEDS_PIN        6
-#define NUMPIXELS      30   // the number of LEDs 
-#define BUTTON_PIN      2   // the number of the pushbutton pin
-int cursor ;                // location of the cursor 
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, LEDS_PIN, NEO_GRB + NEO_KHZ800);
+const int BUTTON_PIN     = 2 ;   // The number of the pushbutton pin
+const int NEOPIXEL_PIN   = 6 ;   // The number of the LED pin
+const int NUMPIXELS      = 30 ;  // The number of LEDs in the LED strip.
+
+int  pixelOffset         = 0 ; 
+int  lastButtonState     = HIGH;  // the previous reading from the input pin
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  strip.begin(); // This initializes the NeoPixel library.  
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // initialize the pushbutton pin as an input
-  cursor = 0 ; 
-  strip.setBrightness(128); // Half brightness
+  strip.begin(); // This initializes the NeoPixel library.
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // initialize the pushbutton pin as an input and high by default.
 }
 
 void loop() {
+  // read the state of the switch into a local variable:
+  int buttonState = digitalRead(BUTTON_PIN);
+  
+  if (buttonState != lastButtonState ) { // Check to see if the button has changed state
+    lastButtonState = buttonState ; // The button has changed state, update the last button state. 
+    if( buttonState == LOW ) { // check to see if the button is currently down., 
+      
+      strip.setPixelColor(pixelOffset, strip.Color(0,0,0)); // Set the perviouse led to black. 
 
-  static long lastDebounceTime = 0;  // the last time the output pin was toggled
-  static int lastButtonState = HIGH ; 
+      pixelOffset ++ ; 
+      if( pixelOffset >= NUMPIXELS ) {
+        pixelOffset = 0 ; 
+      }
 
-  int reading = digitalRead(BUTTON_PIN);  // read the state of the pushbutton value
-  if( reading != lastButtonState ) { 
-    lastButtonState = reading ; 
-    if (reading == LOW && millis() - lastDebounceTime > 200 ) {             // If the button has been pressed,       
-      lastDebounceTime = millis(); 
-      cursor++;                           // Change the pattern.
+      strip.setPixelColor(pixelOffset, strip.Color(150,0,0)); // RED 
+      strip.show(); // This sends the updated pixel color to the hardware. 
     }
   }
-  if( cursor > strip.numPixels() ) {
-    // Reset the LEDs 
-    for( cursor = 0; cursor <= strip.numPixels(); cursor++ ) {
-      strip.setPixelColor(cursor, strip.Color(0,0,0)); 
-    }
-    cursor = 0 ; 
-  }
-
- strip.setPixelColor(cursor, strip.Color(255,0,0)); 
- strip.show(); 
-
 }
+
 
 
